3057afbeb7148899c33d47e792f6c481cde6b362
jhavens <jhavens12@gmail.com>
2018-02-14 15:25:20 -0500

    Graph

---
+++ Main.py
@@ -0,0 +1,36 @@
+from pprint import pprint
+import json
+import matplotlib.pyplot as plt
+import pylab
+import matplotlib.dates as mdates
+from datetime import datetime
+
+with open('HR_Data.json') as f:
+    HR_Dict = json.load(f)
+
+HR_final = {}
+
+for x in sorted(HR_Dict['objects'][0]['rows']):
+    y = x[0].split('.')
+    time_stamp = datetime.strptime(y[0], "%Y-%m-%d %H:%M:%S")
+    HR_final[time_stamp] = x[1]
+    #pprint(x)
+
+x_list = []
+y_list = []
+
+for value in HR_final:
+    if HR_final[value] != 0:
+        x_list.append(value)
+        y_list.append(HR_final[value])
+
+print(len(HR_final.keys()))
+
+plt.plot(x_list,y_list,label=("HR"))
+
+plt.style.use('dark_background')
+plt.rcParams['lines.linewidth'] = 1
+plt.ylim(ymin=0)
+#plt.title('Running Total - ' + str(input1) + ' days, Unit: ' + input2)
+plt.legend()
+plt.show()
